1f74360f8d2d65ca7a08dfae317c19fbd4104278
Albertoq10 <albertoquintana8155@gmail.com>
2026-08-01 17:16:32 +0200

    Funcionamiento de robot sin camara ni sensor gas

--- Codigo_principal/Codigo_principal.ino
+++ Codigo_principal/Codigo_principal.ino
@@ -12,11 +12,9 @@
 #define I2C_SDA 21
 #define I2C_SCL 22
 
-//XSHUT para cada VL53L0X
 #define XSHUT_FRONT 25
 #define XSHUT_RIGHT 26
 #define XSHUT_LEFT  27
-
 
 #define TOF_FRONT_ADDR 0x30
 #define TOF_RIGHT_ADDR 0x31
@@ -24,7 +22,6 @@
 
 const char* WIFI_SSID = "iPhoneBETO";
 const char* WIFI_PASSWORD = "12345678";
-
 const char* SERVER_BASE_URL = "http://172.20.10.7:6000";
 
 Adafruit_AHTX0 dht;
@@ -39,66 +36,94 @@ bool ina_ok = false;
 
 bool tof_front_ok = false;
 bool tof_right_ok = false;
-bool tof_left_ok = false;
+bool tof_left_ok  = false;
 
+const int enA = 32;
+const int in1 = 13;
+const int in2 = 14;
+
+const int enB = 33;
+const int in3 = 18;
+const int in4 = 19;
+
+const int freq = 1000;
+const int resolution = 8;
+
+const int pwmChannelA = 0;
+const int pwmChannelB = 1;
+
+const int SPEED_NORMAL = 100;
+const int SPEED_TURN   = 180;
+const int SPEED_BACK   = 150;
+
+const int FRONT_LIMIT_MM = 500;
+const int SIDE_LIMIT_MM  = 300;
+
+enum RobotState {
+  STATE_NORMAL,
+  STATE_CORRECT_LEFT,
+  STATE_CORRECT_RIGHT,
+  STATE_BACKWARD,
+  STATE_TURN_RIGHT,
+  STATE_TURN_LEFT
+};
+
+RobotState robotState = STATE_NORMAL;
+
+unsigned long stateStartTime = 0;
+unsigned long lastSensorPrint = 0;
 unsigned long lastHttpSend = 0;
+
+const unsigned long CORRECT_TIME = 250;
+const unsigned long BACK_TIME = 1200;
+const unsigned long ESCAPE_TURN_TIME = 1300;
+const unsigned long SENSOR_PRINT_TIME = 300;
 const unsigned long HTTP_SEND_INTERVAL = 2000;
+
+bool escapeTurnRight = true;
 
 WiFiClient wifi;
 
+int frontDistanceMm = -1;
+int rightDistanceMm = -1;
+int leftDistanceMm  = -1;
 
-void initToFSensors() {
-  pinMode(XSHUT_FRONT, OUTPUT);
-  pinMode(XSHUT_RIGHT, OUTPUT);
-  pinMode(XSHUT_LEFT, OUTPUT);
+bool frontTofValid = false;
+bool rightTofValid = false;
+bool leftTofValid  = false;
 
-  // Apagar todos los sensores
-  digitalWrite(XSHUT_FRONT, LOW);
-  digitalWrite(XSHUT_RIGHT, LOW);
-  digitalWrite(XSHUT_LEFT, LOW);
-  delay(100);
+float temperatureC = NAN;
+float humidityPct = NAN;
 
+float shuntvoltage = 0;
+float busvoltage = 0;
+float current_mA = 0;
+float loadvoltage = 0;
+float power_mW = 0;
 
-  // Sensor frontal
-
-  digitalWrite(XSHUT_FRONT, HIGH);
-  delay(100);
-
-  if (!tofFront.begin(TOF_FRONT_ADDR, false, &Wire)) {
-    Serial.println("No se encontro el VL53L0X frontal. Revisa conexiones.");
-    tof_front_ok = false;
-  } else {
-    Serial.println("VL53L0X frontal detectado correctamente en 0x30.");
-    tof_front_ok = true;
-  }
-
-  
-
-  digitalWrite(XSHUT_RIGHT, HIGH);
-  delay(100);
-
-  if (!tofRight.begin(TOF_RIGHT_ADDR, false, &Wire)) {
-    Serial.println("No se encontro el VL53L0X derecho. Revisa conexiones.");
-    tof_right_ok = false;
-  } else {
-    Serial.println("VL53L0X derecho detectado correctamente en 0x31.");
-    tof_right_ok = true;
-  }
-
- 
-  digitalWrite(XSHUT_LEFT, HIGH);
-  delay(100);
-
-  if (!tofLeft.begin(TOF_LEFT_ADDR, false, &Wire)) {
-    Serial.println("No se encontro el VL53L0X izquierdo. Revisa conexiones.");
-    tof_left_ok = false;
-  } else {
-    Serial.println("VL53L0X izquierdo detectado correctamente en 0x32.");
-    tof_left_ok = true;
-  }
+void startState(RobotState newState) {
+  robotState = newState;
+  stateStartTime = millis();
 }
 
-
+const char* getRobotStateName() {
+  switch (robotState) {
+    case STATE_NORMAL:
+      return "normal";
+    case STATE_CORRECT_LEFT:
+      return "correct_left";
+    case STATE_CORRECT_RIGHT:
+      return "correct_right";
+    case STATE_BACKWARD:
+      return "backward";
+    case STATE_TURN_RIGHT:
+      return "turn_right";
+    case STATE_TURN_LEFT:
+      return "turn_left";
+    default:
+      return "unknown";
+  }
+}
 
 int readToF(Adafruit_VL53L0X &sensor, bool sensor_ok, bool &valid) {
   if (!sensor_ok) {
@@ -118,38 +143,218 @@ int readToF(Adafruit_VL53L0X &sensor, bool sensor_ok, bool &valid) {
   }
 }
 
+void stopMotors() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+
+  ledcWrite(enA, 0);
+  ledcWrite(enB, 0);
+}
+
+void moveForward(int speedValue) {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
+  ledcWrite(enA, speedValue);
+  ledcWrite(enB, speedValue);
+}
+
+void moveBackward(int speedValue) {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  ledcWrite(enA, speedValue);
+  ledcWrite(enB, speedValue);
+}
+
+void turnRight(int speedValue) {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
+  ledcWrite(enA, speedValue);
+  ledcWrite(enB, speedValue);
+}
+
+void turnLeft(int speedValue) {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+
+  ledcWrite(enA, speedValue);
+  ledcWrite(enB, speedValue);
+}
+
+void initToFSensors() {
+  pinMode(XSHUT_FRONT, OUTPUT);
+  pinMode(XSHUT_RIGHT, OUTPUT);
+  pinMode(XSHUT_LEFT, OUTPUT);
+
+  digitalWrite(XSHUT_FRONT, LOW);
+  digitalWrite(XSHUT_RIGHT, LOW);
+  digitalWrite(XSHUT_LEFT, LOW);
+  delay(100);
+
+  digitalWrite(XSHUT_FRONT, HIGH);
+  delay(100);
+  tof_front_ok = tofFront.begin(TOF_FRONT_ADDR, false, &Wire);
+  Serial.println(tof_front_ok ? "VL53L0X frontal OK" : "VL53L0X frontal NO detectado");
+
+  digitalWrite(XSHUT_RIGHT, HIGH);
+  delay(100);
+  tof_right_ok = tofRight.begin(TOF_RIGHT_ADDR, false, &Wire);
+  Serial.println(tof_right_ok ? "VL53L0X derecho OK" : "VL53L0X derecho NO detectado");
+
+  digitalWrite(XSHUT_LEFT, HIGH);
+  delay(100);
+  tof_left_ok = tofLeft.begin(TOF_LEFT_ADDR, false, &Wire);
+  Serial.println(tof_left_ok ? "VL53L0X izquierdo OK" : "VL53L0X izquierdo NO detectado");
+}
+
+void readEnvironmentalSensors() {
+  if (dht_ok) {
+    sensors_event_t humidity, temp;
+    dht.getEvent(&humidity, &temp);
+
+    temperatureC = temp.temperature;
+    humidityPct = humidity.relative_humidity;
+  }
+
+  if (ina_ok) {
+    shuntvoltage = ina219.getShuntVoltage_mV();
+    busvoltage = ina219.getBusVoltage_V();
+    current_mA = ina219.getCurrent_mA();
+    power_mW = ina219.getPower_mW();
+    loadvoltage = busvoltage + (shuntvoltage / 1000);
+  }
+}
+
+void sendDataToFlask() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi desconectado");
+    return;
+  }
+
+  readEnvironmentalSensors();
+
+  StaticJsonDocument<1024> doc;
+
+  doc["device_id"] = DEVICE_ID;
+  doc["robot_state"] = getRobotStateName();
+
+  if (dht_ok) {
+    doc["temperature_c"] = temperatureC;
+    doc["humidity_pct"] = humidityPct;
+  }
+
+  if (tof_front_ok) {
+    doc["front_distance_mm"] = frontDistanceMm;
+    doc["front_tof_valid"] = frontTofValid;
+  }
+
+  if (tof_right_ok) {
+    doc["right_distance_mm"] = rightDistanceMm;
+    doc["right_tof_valid"] = rightTofValid;
+  }
+
+  if (tof_left_ok) {
+    doc["left_distance_mm"] = leftDistanceMm;
+    doc["left_tof_valid"] = leftTofValid;
+  }
+
+  if (ina_ok) {
+    doc["bus_voltage_v"] = busvoltage;
+    doc["shunt_voltage_mv"] = shuntvoltage;
+    doc["load_voltage_v"] = loadvoltage;
+    doc["current_ma"] = current_mA;
+    doc["power_mw"] = power_mW;
+  }
+
+  String json_string;
+  serializeJson(doc, json_string);
+
+  String url = String(SERVER_BASE_URL) + "/sensor_values";
+
+  Serial.println();
+  Serial.println("========== ENVIO A FLASK ==========");
+  Serial.print("URL: ");
+  Serial.println(url);
+  Serial.print("JSON: ");
+  Serial.println(json_string);
+
+  HTTPClient http;
+  http.begin(wifi, url);
+  http.setConnectTimeout(1000);
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponseCode = http.POST(json_string);
+
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+    Serial.print("HTTP ");
+    Serial.print(httpResponseCode);
+    Serial.print(": ");
+    Serial.println(response);
+  } else {
+    Serial.print("Error HTTP: ");
+    Serial.println(http.errorToString(httpResponseCode));
+  }
+
+  http.end();
+
+  Serial.println("===================================");
+}
+
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
   Serial.println();
-  Serial.println("DHT20 + 3 VL53L0X + INA219 - Envio de datos a Flask e InfluxDB");
+  Serial.println("Robot ESP32 + 3 VL53L0X + L298N + DHT20 + INA219 + Flask");
 
   Wire.begin(I2C_SDA, I2C_SCL);
 
-  
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  ledcAttachChannel(enA, freq, resolution, pwmChannelA);
+  ledcAttachChannel(enB, freq, resolution, pwmChannelB);
+
+  stopMotors();
+
   if (!dht.begin()) {
-    Serial.println("No se encontro el sensor DHT20/AHT20. Revisa conexiones.");
+    Serial.println("No se encontro el sensor DHT20/AHT20.");
     dht_ok = false;
   } else {
     Serial.println("Sensor DHT20/AHT20 detectado correctamente.");
     dht_ok = true;
   }
 
-
   if (!ina219.begin()) {
     Serial.println("Failed to find INA219 chip");
     ina_ok = false;
   } else {
-    ina219.setCalibration_16V_400mA();
+    ina219.setCalibration_32V_2A();
     Serial.println("INA219 detectado correctamente.");
     ina_ok = true;
   }
 
-  
   initToFSensors();
 
-  
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.println("Conectando a WiFi...");
@@ -161,182 +366,137 @@ void setup() {
   Serial.println("Conectado a WiFi");
   Serial.print("IP ESP32: ");
   Serial.println(WiFi.localIP());
+
+  Serial.println("Sistema listo.");
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    unsigned long now = millis();
+  unsigned long now = millis();
 
-    if (now - lastHttpSend >= HTTP_SEND_INTERVAL) {
-      lastHttpSend = now;
+  frontDistanceMm = readToF(tofFront, tof_front_ok, frontTofValid);
+  rightDistanceMm = readToF(tofRight, tof_right_ok, rightTofValid);
+  leftDistanceMm  = readToF(tofLeft,  tof_left_ok,  leftTofValid);
 
-      float temperatureC = NAN;
-      float humidityPct = NAN;
+  if (now - lastSensorPrint >= SENSOR_PRINT_TIME) {
+    lastSensorPrint = now;
 
-      int frontDistanceMm = -1;
-      int rightDistanceMm = -1;
-      int leftDistanceMm = -1;
+    Serial.println("----- Lecturas ToF -----");
 
-      bool frontTofValid = false;
-      bool rightTofValid = false;
-      bool leftTofValid = false;
+    Serial.print("Frontal: ");
+    Serial.print(frontDistanceMm);
+    Serial.print(" mm | valido: ");
+    Serial.println(frontTofValid);
 
-      float shuntvoltage = 0;
-      float busvoltage = 0;
-      float current_mA = 0;
-      float loadvoltage = 0;
-      float power_mW = 0;
+    Serial.print("Derecho: ");
+    Serial.print(rightDistanceMm);
+    Serial.print(" mm | valido: ");
+    Serial.println(rightTofValid);
 
-  
-      if (dht_ok) {
-        sensors_event_t humidity, temp;
-        dht.getEvent(&humidity, &temp);
+    Serial.print("Izquierdo: ");
+    Serial.print(leftDistanceMm);
+    Serial.print(" mm | valido: ");
+    Serial.println(leftTofValid);
 
-        temperatureC = temp.temperature;
-        humidityPct = humidity.relative_humidity;
-      }
-
-    
-      frontDistanceMm = readToF(tofFront, tof_front_ok, frontTofValid);
-      rightDistanceMm = readToF(tofRight, tof_right_ok, rightTofValid);
-      leftDistanceMm  = readToF(tofLeft,  tof_left_ok,  leftTofValid);
-
-      
-      if (ina_ok) {
-        shuntvoltage = ina219.getShuntVoltage_mV();
-        busvoltage = ina219.getBusVoltage_V();
-        current_mA = ina219.getCurrent_mA();
-        power_mW = ina219.getPower_mW();
-        loadvoltage = busvoltage + (shuntvoltage / 1000);
-      }
-
-  
-   
-      StaticJsonDocument<768> doc;
-
-      doc["device_id"] = DEVICE_ID;
-
-      if (dht_ok) {
-        doc["temperature_c"] = temperatureC;
-        doc["humidity_pct"] = humidityPct;
-      }
-
-      if (tof_front_ok) {
-        doc["front_distance_mm"] = frontDistanceMm;
-        doc["front_tof_valid"] = frontTofValid;
-      }
-
-      if (tof_right_ok) {
-        doc["right_distance_mm"] = rightDistanceMm;
-        doc["right_tof_valid"] = rightTofValid;
-      }
-
-      if (tof_left_ok) {
-        doc["left_distance_mm"] = leftDistanceMm;
-        doc["left_tof_valid"] = leftTofValid;
-      }
-
-      if (ina_ok) {
-        doc["bus_voltage_v"] = busvoltage;
-        doc["shunt_voltage_mv"] = shuntvoltage;
-        doc["load_voltage_v"] = loadvoltage;
-        doc["current_ma"] = current_mA;
-        doc["power_mw"] = power_mW;
-      }
-
-      String json_string;
-      serializeJson(doc, json_string);
-
-     
-      Serial.println();
-      Serial.println("========== LECTURA DE SENSORES ==========");
-
-      if (dht_ok) {
-        Serial.print("Temperatura: ");
-        Serial.print(temperatureC);
-        Serial.println(" °C");
-
-        Serial.print("Humedad: ");
-        Serial.print(humidityPct);
-        Serial.println(" %");
-      }
-
-      Serial.println("--- VL53L0X ---");
-
-      Serial.print("Frontal: ");
-      Serial.print(frontDistanceMm);
-      Serial.print(" mm | valido: ");
-      Serial.println(frontTofValid ? "SI" : "NO");
-
-      Serial.print("Derecho: ");
-      Serial.print(rightDistanceMm);
-      Serial.print(" mm | valido: ");
-      Serial.println(rightTofValid ? "SI" : "NO");
-
-      Serial.print("Izquierdo: ");
-      Serial.print(leftDistanceMm);
-      Serial.print(" mm | valido: ");
-      Serial.println(leftTofValid ? "SI" : "NO");
-
-      if (ina_ok) {
-        Serial.println("--- INA219 ---");
-
-        Serial.print("Bus voltage: ");
-        Serial.print(busvoltage);
-        Serial.println(" V");
-
-        Serial.print("Shunt voltage: ");
-        Serial.print(shuntvoltage);
-        Serial.println(" mV");
-
-        Serial.print("Load voltage: ");
-        Serial.print(loadvoltage);
-        Serial.println(" V");
-
-        Serial.print("Current: ");
-        Serial.print(current_mA);
-        Serial.println(" mA");
-
-        Serial.print("Power: ");
-        Serial.print(power_mW);
-        Serial.println(" mW");
-      }
-
-      Serial.print("URL: ");
-      String url = String(SERVER_BASE_URL) + "/sensor_values";
-      Serial.println(url);
-
-      Serial.print("Enviando JSON: ");
-      Serial.println(json_string);
-
-    
-      HTTPClient http;
-
-      http.begin(wifi, url);
-      http.setConnectTimeout(5000);
-      http.addHeader("Content-Type", "application/json");
-
-      int httpResponseCode = http.POST(json_string);
-
-      if (httpResponseCode > 0) {
-        String response = http.getString();
-
-        Serial.print("HTTP ");
-        Serial.print(httpResponseCode);
-        Serial.print(": ");
-        Serial.println(response);
-      } else {
-        Serial.print("Error HTTP: ");
-        Serial.println(http.errorToString(httpResponseCode));
-      }
-
-      http.end();
-
-      Serial.println("==========================================");
-    }
-  } else {
-    Serial.println("WiFi desconectado");
+    Serial.print("Estado: ");
+    Serial.println(getRobotStateName());
   }
 
-  delay(100);
+  bool frontBlocked = frontTofValid && frontDistanceMm < FRONT_LIMIT_MM;
+
+  bool rightTooClose = rightTofValid && rightDistanceMm < SIDE_LIMIT_MM;
+  bool leftTooClose  = leftTofValid  && leftDistanceMm  < SIDE_LIMIT_MM;
+
+  bool rightFree = rightTofValid && rightDistanceMm > SIDE_LIMIT_MM;
+  bool leftFree  = leftTofValid  && leftDistanceMm  > SIDE_LIMIT_MM;
+
+  if (frontBlocked && robotState == STATE_NORMAL) {
+    stopMotors();
+    startState(STATE_BACKWARD);
+  }
+
+  if (robotState == STATE_CORRECT_LEFT) {
+    turnLeft(SPEED_TURN);
+
+    if (now - stateStartTime >= CORRECT_TIME) {
+      stopMotors();
+      startState(STATE_NORMAL);
+    }
+  }
+  else if (robotState == STATE_CORRECT_RIGHT) {
+    turnRight(SPEED_TURN);
+
+    if (now - stateStartTime >= CORRECT_TIME) {
+      stopMotors();
+      startState(STATE_NORMAL);
+    }
+  }
+  else if (robotState == STATE_BACKWARD) {
+    moveBackward(SPEED_BACK);
+
+    if (now - stateStartTime >= BACK_TIME) {
+      stopMotors();
+
+      if (rightFree && !leftFree) {
+        startState(STATE_TURN_RIGHT);
+      } 
+      else if (leftFree && !rightFree) {
+        startState(STATE_TURN_LEFT);
+      } 
+      else if (rightFree && leftFree) {
+        if (rightDistanceMm > leftDistanceMm) {
+          startState(STATE_TURN_RIGHT);
+        } else {
+          startState(STATE_TURN_LEFT);
+        }
+      } 
+      else {
+        if (escapeTurnRight) {
+          startState(STATE_TURN_RIGHT);
+        } else {
+          startState(STATE_TURN_LEFT);
+        }
+
+        escapeTurnRight = !escapeTurnRight;
+      }
+    }
+  }
+  else if (robotState == STATE_TURN_RIGHT) {
+    turnRight(SPEED_TURN);
+
+    if (now - stateStartTime >= ESCAPE_TURN_TIME) {
+      stopMotors();
+      startState(STATE_NORMAL);
+    }
+  }
+  else if (robotState == STATE_TURN_LEFT) {
+    turnLeft(SPEED_TURN);
+
+    if (now - stateStartTime >= ESCAPE_TURN_TIME) {
+      stopMotors();
+      startState(STATE_NORMAL);
+    }
+  }
+  else {
+    if (!frontBlocked && !rightTooClose && !leftTooClose) {
+      moveForward(SPEED_NORMAL);
+    }
+    else if (!frontBlocked && rightTooClose && !leftTooClose) {
+      startState(STATE_CORRECT_LEFT);
+    }
+    else if (!frontBlocked && leftTooClose && !rightTooClose) {
+      startState(STATE_CORRECT_RIGHT);
+    }
+    else if (!frontBlocked && rightTooClose && leftTooClose) {
+      moveForward(70);
+    }
+    else {
+      stopMotors();
+      startState(STATE_BACKWARD);
+    }
+  }
+
+  if (now - lastHttpSend >= HTTP_SEND_INTERVAL) {
+    lastHttpSend = now;
+    sendDataToFlask();
+  }
 }
--- Codigo_principal/flask_main.py
+++ Codigo_principal/flask_main.py
@@ -7,7 +7,6 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 
 app = Flask("servidor_flask")
 
-
 INFLUX_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
 INFLUX_TOKEN = "YQ3PP1vQBgVwNJjT0zkbos6WF1PIrwAjPshrpD6qK4fOXrPhOFVXsFTRpKMm7qlTsbh4mnYtSRVdaPyd5a0Lsg=="
 INFLUX_ORG = "Student"
@@ -35,16 +34,12 @@ def sensor_values():
 
     data = request.get_json(force=True, silent=True) or {}
 
-
     device_id = data.get("device_id", "unknown")
-
+    robot_state = data.get("robot_state", "unknown")
 
     temperature = data.get("temperature_c")
     humidity = data.get("humidity_pct")
 
-   
-    # 3 VL53L0X
-    
     front_distance = data.get("front_distance_mm")
     front_tof_valid = data.get("front_tof_valid", False)
 
@@ -54,22 +49,20 @@ def sensor_values():
     left_distance = data.get("left_distance_mm")
     left_tof_valid = data.get("left_tof_valid", False)
 
-    
     old_tof_valid = data.get("tof_valid")
     if old_tof_valid is not None and "front_tof_valid" not in data:
         front_tof_valid = old_tof_valid
 
- 
     bus_voltage = data.get("bus_voltage_v")
     shunt_voltage = data.get("shunt_voltage_mv")
     load_voltage = data.get("load_voltage_v")
     current_ma = data.get("current_ma")
     power_mw = data.get("power_mw")
 
-
     print("----- Datos recibidos -----")
     print(f"Tiempo: {datetime.now()}")
     print(f"Device ID: {device_id}")
+    print(f"Estado robot: {robot_state}")
 
     print("--- DHT20 / AHT20 ---")
     print(f"Temperatura: {temperature} °C")
@@ -95,6 +88,7 @@ def sensor_values():
         point = (
             Point("robot_sensors")
             .tag("device_id", device_id)
+            .tag("robot_state", robot_state)
             .time(datetime.now(timezone.utc), WritePrecision.NS)
         )
 
@@ -104,7 +98,6 @@ def sensor_values():
         if humidity is not None:
             point = point.field("humidity_pct", float(humidity))
 
-    
         if front_distance is not None:
             point = point.field("front_distance_mm", int(front_distance))
 
@@ -120,7 +113,6 @@ def sensor_values():
 
         point = point.field("left_tof_valid", bool(left_tof_valid))
 
-       
         if bus_voltage is not None:
             point = point.field("bus_voltage_v", float(bus_voltage))
 
@@ -136,7 +128,6 @@ def sensor_values():
         if power_mw is not None:
             point = point.field("power_mw", float(power_mw))
 
-        
         write_api.write(
             bucket=INFLUX_BUCKET,
             org=INFLUX_ORG,
@@ -146,6 +137,7 @@ def sensor_values():
         return jsonify({
             "status": "ok",
             "device_id": device_id,
+            "robot_state": robot_state,
             "message": "Datos recibidos y enviados a InfluxDB",
             "received": {
                 "temperature_c": temperature,
